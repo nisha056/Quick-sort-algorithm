2e310475eee7ae80ad334d912cef645e6b17ad8b
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-11-13 22:27:21 +0545

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -8,6 +8,8 @@ QuickSort::QuickSort()
 	outlineBox.setOutlineThickness(10);
 	outlineBox.setFillColor(sf::Color::Transparent);
 	infoText.setCharacterSize(30);
+	text1.setCharacterSize(20);
+	text2.setCharacterSize(20);
 	if (!font.loadFromFile("DejaVuSans.ttf"))
     {
         std::cout<<"kkk";
@@ -16,6 +18,15 @@ QuickSort::QuickSort()
 	infoText.setFillColor(sf::Color::Black);
 	infoText.setPosition(sf::Vector2f(50, 50));
 	infoText.setString(" Press 'S' to load data ... ");
+	text1.setFont(font);
+	text1.setFillColor(sf::Color::Blue);
+	text1.setPosition(sf::Vector2f(800,50));
+	text1.setString(" Pivot");
+	text2.setFont(font);
+	text2.setFillColor(sf::Color::Green);
+	text2.setPosition(sf::Vector2f(800,100));
+	text2.setString(" Sorted");
+
 
 }
 
@@ -37,35 +48,38 @@ void QuickSort::setData(int height[])
 	std::cout << "\n";
 
 }
-// last element is taken as pivot
 
-int QuickSort::partition(int height[],int low,int high)
+    int QuickSort::partition(int height[],int low,int high)
 {
-    int i=low,pivot=height[high];
-    for(int j=low;j<high;j++)
+    int current=low,pivot=height[high];
+    for(int index=low;index<high;index++)
     {
-        if(height[j]<= pivot)
+        if(height[index]<= pivot)
         {
-            int temp=height[i];
-            height[i]=height[j];
-            height[j]=temp;
-           // height.draw(2,i,j);
-            i++;
+            int temp=height[current];
+            height[current]=height[index];
+            height[index]=temp;
+            sf::sleep(sf::milliseconds(100));
+            current++;
         }
     }
-    int tem=height[i];
-    height[i]=height[high];
+    int tem=height[current];
+    height[current]=height[high];
     height[high]=tem;
-    return i;
-
-}
+    sf::sleep(sf::milliseconds(100));
+    return current;
+    }
 void QuickSort::quicksort(int height[],int low,int high)
 {
-    //std::cout<<"Sorting the data";
+    std::cout<<"Sorting the data";
     if(low<high)
     {
+        sf::sleep(sf::milliseconds(100));
         int notation=partition(height,low,high);
+        //sf::sleep(sf::milliseconds(100));
+
         quicksort(height,low,notation-1);
+        //sf::sleep(sf::milliseconds(100));
         quicksort(height,notation+1,high);
     }
 
@@ -73,14 +87,19 @@ void QuickSort::quicksort(int height[],int low,int high)
 void QuickSort::sort(int height[])
 {
 	std::cout << "Sorting Data ";
-	quicksort(height,0,9);
+    quicksort(height,0,9);
 }
 
 void QuickSort::draw(sf::RenderWindow& window)
 {
 	window.draw(outlineBox);
 	window.draw(infoText);
+	window.draw(text1);
+	window.draw(text2);
 	for (int i = 0; i < 10; i++) {
 		window.draw(recn[i]);
 	}
+
 }
+
+
