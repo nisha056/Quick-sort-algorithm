5fb38bc67b74ba31f71bafe9e1ed7805904a92e9
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-11-18 18:57:33 +0545

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -3,14 +3,8 @@
 #include <SFML/Graphics.hpp>
 class QuickSort
 {
-public:
-    QuickSort();
-    void setData(int height[]);
-    void sort(int[]);
-    void quicksort(int[],int,int);
-    int partition(int[],int,int);
-    void draw(sf::RenderWindow&); // For drawing in screen
 private:
+    sf::RenderWindow window;
     sf::RectangleShape recn[10];
     sf::RectangleShape outlineBox;
     sf::RectangleShape height[];
@@ -18,4 +12,11 @@ private:
     sf::Text infoText;
     sf::Text text1;
     sf::Text text2;
+
+public:
+    QuickSort();
+    void setData(int height[]);
+    int partition(int[],int,int);
+    void quicksort(int[],int,int);
+    void draw(sf::RenderWindow&);// For drawing in screen
 };
