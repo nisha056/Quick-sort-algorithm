c869ea6133a17ca8921fbca393765bf11fe9e448
Ruja Awal <59213252+awalruja@users.noreply.github.com>
2020-11-25 09:26:46 +0545

    Update Visualizer.cpp

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -1,9 +1,22 @@
 #include "Visualizer.h"
 
-Visualizer::Visualizer():window(sf::VideoMode(1000,900),"Visualizer")
-{
 
-}
+Visualizer::Visualizer():window(sf::VideoMode(800,600),"Visualizer"),
+
+setDataButton("Set",sf::Vector2f(100,50),20,sf::Color::Green,sf::Color::Black),
+sortButton("Sort",sf::Vector2f(100,50),20,sf::Color::Green,sf::Color::Black)
+{
+    if(!font.loadFromFile("DejaVuSans.ttf"))
+    {
+        //error
+    }
+    setDataButton.setPosition(sf::Vector2f(350,25));
+    setDataButton.setFont(font);
+    sortButton.setPosition(sf::Vector2f(475,25));
+    sortButton.setFont(font);
+    }
+
+
 
 void Visualizer::run()
 {
@@ -19,31 +32,49 @@ void Visualizer::handleUserInput()
     sf::Event event;
     while (window.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
+        if (event.type == sf::Event::Closed){
             window.close();
-        else if (event.type == sf::Event::EventType::KeyPressed)
+        }
+        else if (event.type == sf::Event::EventType::MouseMoved)
+        {
+            if(setDataButton.isMouseOver(window))
             {
-        if (event.key.code == sf::Keyboard::S)
-        {
-            Q.setData(height);
-           
+                setDataButton.setBackColor(sf::Color::Red);
 
-       }
-       if (event.key.code == sf::Keyboard::Enter)
-        {
-            Q.quicksort(height,0,9);
+            }
+           else
+            {
+                setDataButton.setBackColor(sf::Color::Green);
+            }
 
-       }
-    }
-    }
+            if(sortButton.isMouseOver(window))
+            {
+                sortButton.setBackColor(sf::Color::Red);
+            }
+            else
+            {
+                sortButton.setBackColor(sf::Color::Green);
+            }
+        }
+        else if (event.type == sf::Event::EventType::MouseButtonPressed)
+        {
+              if (setDataButton.isMouseOver(window))
+              {
+                  Q.setData(height);
+              }
+              else if (sortButton.isMouseOver(window))
+              {
+                  Q.quicksort(height,0,9);
+              }
+          }
+     }
 }
-
 void Visualizer::render()
 {
     window.clear(sf::Color::White);
-
+    setDataButton.drawTo(window);
+    sortButton.drawTo(window);
     Q.draw(window);
 
     window.display();
 }
-
