9e55f0c43c132842c04e5dccd84a219727a39aec
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-12-08 12:21:51 +0545

    Update QuickSort.h

--- QuickSort.h
+++ QuickSort.h
@@ -7,8 +7,12 @@ class QuickSort
 {
 public:
     QuickSort();
-    void setData(int height[]);
-    bool sort();
+    void setData(int data[]);
+    int partition (int[],int,int);
+    bool quicksort (int [],int ,int );
+    int greatest(int arr[]);
+    bool sort(int data[],int ,int);
+    void lastpivot(int data[],int,int);
     void draw(sf::RenderWindow&);// For drawing in screen
 private:
     sf::RectangleShape recn[10];
@@ -16,4 +20,7 @@ private:
     sf::RectangleShape outlineBox1;
     sf::RectangleShape outlineBox2;
     void equateRectangle(sf::RectangleShape*,sf::RectangleShape*);
+    std::string comma= "";
+   // std::string separate = "";
+    //std::string part = "";
 };
