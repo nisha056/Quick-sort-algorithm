53024a08da1753460cfa3eae8dda042a1340ccb6
Nisha Sharma <60255047+nisha056@users.noreply.github.com>
2020-12-11 16:35:48 +0545

    Update Visualizer.cpp

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -1,33 +1,72 @@
 #include "Visualizer.h"
 
 
-Visualizer::Visualizer():window(sf::VideoMode(1000,900),"Visualizer"),
+void Visualizer::splitInt(std::string str)
+{
+        std::string num = "";
+        datas.clear();
+        for (int i = 0; i < str.length(); ++i)
+        {
+            if (str[i] == ' ' && i == str.length())
+                break;
+            else if (str[i] == ' ') {
+                int t = stoi(num);
+                datas.push_back(t);
+                num = "";
+            }
+            else
+                num = num + str[i];
+        }
+        if (str[str.length() - 1] != ' ') {
+            int y = stoi(num);
+            datas.push_back(y);
+        }
 
-setDataButton("Set",sf::Vector2f(120,50),20,sf::Color::Green,sf::Color::Black),
-sortButton("Sort",sf::Vector2f(120,50),20,sf::Color::Green,sf::Color::Black)
+}
+
+void Visualizer::printData(std::vector<int> datas)
+{
+    std::cout<<" The data we enterd is : ";
+    for (const auto data : datas) {
+        std::cout << data<<",";
+    }
+    std::cout << std::endl;
+}
+
+Visualizer::Visualizer() :window(sf::VideoMode(1000, 900), "Visualizer"),
+
+setDataButton("Set", sf::Vector2f(120, 50), 20, sf::Color::Green, sf::Color::Black),
+sortButton("Sort", sf::Vector2f(120, 50), 20, sf::Color::Green, sf::Color::Black),
+pauseButton("Pause/Play", sf::Vector2f(200, 50), 20, sf::Color::Green, sf::Color::Black), textbox1(20, sf::Color::Red, false)
 {
     isSorting = true;
-    std::thread sortThread (&Visualizer :: sort, this);
+    std::thread sortThread(&Visualizer::sort, this);
     sortThread.detach(); // it destroys sorting thread
-    if(!font.loadFromFile("DejaVuSans.ttf"))
+    if (!font.loadFromFile("DejaVuSans.ttf"))
     {
-        //error
     }
-    setDataButton.setPosition(sf::Vector2f(55,55));
+    setDataButton.setPosition(sf::Vector2f(55, 55));
     setDataButton.setFont(font);
-    sortButton.setPosition(sf::Vector2f(825,55));
+    sortButton.setPosition(sf::Vector2f(825, 55));
     sortButton.setFont(font);
-    }
+    pauseButton.setPosition(sf::Vector2f(450, 775));
+    pauseButton.setFont(font);
+    textbox1.setFont(font);
+    textbox1.setPosition(sf::Vector2f(200, 80));
+    textbox1.setLimit(true, 50);
+
+
+}
 
 
 
 void Visualizer::run()
 {
     std::cout << "Running \n";
-	while (window.isOpen()) {
-		handleUserInput();
-		render();
-	}
+    while (window.isOpen()) {
+        handleUserInput();
+        render();
+    }
 }
 
 void Visualizer::handleUserInput()
@@ -35,22 +74,35 @@ void Visualizer::handleUserInput()
     sf::Event event;
     while (window.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed){
+        if (event.type == sf::Event::Closed) {
             window.close();
+        }
+        else if (event.type == sf::Event::TextEntered){
+             textbox1.typedOn(event);
+            break;
         }
         else if (event.type == sf::Event::EventType::MouseMoved)
         {
-            if(setDataButton.isMouseOver(window))
+            if (setDataButton.isMouseOver(window))
             {
                 setDataButton.setBackColor(sf::Color::Red);
 
             }
-           else
+            else
             {
                 setDataButton.setBackColor(sf::Color::Green);
             }
+            if (pauseButton.isMouseOver(window))
+            {
+                pauseButton.setBackColor(sf::Color::Red);
 
-            if(sortButton.isMouseOver(window))
+            }
+            else
+            {
+                pauseButton.setBackColor(sf::Color::Green);
+            }
+
+            if (sortButton.isMouseOver(window))
             {
                 sortButton.setBackColor(sf::Color::Red);
             }
@@ -61,16 +113,30 @@ void Visualizer::handleUserInput()
         }
         else if (event.type == sf::Event::EventType::MouseButtonPressed)
         {
-              if (setDataButton.isMouseOver(window))
-              {
-                  Q.setData(data);
-              }
-              else if (sortButton.isMouseOver(window))
-              {
-                  isSorting = false;
-              }
-          }
-     }
+            if (setDataButton.isMouseOver(window))
+            {
+                if (textbox1.getInput() != "") {
+                    splitInt(textbox1.getInput());
+                }
+                printData(datas);
+                Q.setData(&datas[0],datas.size());
+            }
+            else if (sortButton.isMouseOver(window))
+            {
+                isSorting = false;
+            }
+            else if (pauseButton.isMouseOver(window)) {
+                Q.isPaused = !Q.isPaused;
+            }
+            else if (textbox1.isMouseOver(window))
+            {
+               textbox1.setSelected(true);
+            }
+            else {
+                textbox1.setSelected(false);
+            }
+        }
+    }
 }
 void Visualizer::render()
 {
@@ -78,16 +144,18 @@ void Visualizer::render()
     setDataButton.drawTo(window);
     sortButton.drawTo(window);
     Q.draw(window);
-
+    textbox1.drawTo(window);
+    pauseButton.drawTo(window);
     window.display();
 }
 void Visualizer::sort()
 {
-    while(window.isOpen())
+    while (window.isOpen())
     {
-         if (!isSorting) {
-            this->isSorting = Q.sort(data,0,9);
+        if (!isSorting) {
+            this->isSorting = Q.sort(&datas[0], 0, datas.size()-1);
             std::cout << isSorting << std::endl;
         }
     }
 }
+
